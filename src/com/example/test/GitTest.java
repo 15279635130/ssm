3772a953e9d0660fd141d12c993325ee08bf0d3d
tom_glb <goodMorning_glb@qq.com>
2019-08-18 10:25:46 +0800

    测试GitTest类修改冲突

--- src/com/example/test/GitTest.java
+++ src/com/example/test/GitTest.java
@@ -2,6 +2,6 @@ package com.example.test;
 
 public class GitTest {
 	public static void main(String[] args) {
-		System.out.println("git test ....");
+		System.out.println("git test by master ....");
 	}
 }
